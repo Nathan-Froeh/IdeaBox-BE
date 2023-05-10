d3421a7ed97e8e9b9c7ac4c5f6387ed58e3825c5
Nathan <nathan.froeh@gmail.com>
2023-05-10 12:06:55 -0600

    add post endpoint

--- app.js
+++ app.js
@@ -8,9 +8,23 @@ import {
 
 const app = express();
 
+app.use(express.json())
+
 app.get("/notes", async (req, res) => {
   const notes = await getNotes();
   res.send(notes)
+})
+
+app.get("/notes/:id", async (req, res) => {
+  const id = req.params.id;
+  const note = await getNote(id);
+  res.send(note)
+})
+
+app.post("/notes", async (req, res) => {
+  const { title, content } = req.body;
+  const note = await createNote(title, content)
+  res.status(201).send(note)
 })
 
 app.use((err, req, res, next) => {
